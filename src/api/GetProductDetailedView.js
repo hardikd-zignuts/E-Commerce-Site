5ab3e0f6fc07dec4024b394182fda33edb7d4c73
hardikd-zignuts <hardikd@zignuts.com>
2023-03-13 13:16:57 +0530

    Get API Call for different file

--- src/api/GetProductDetailedView.js
+++ src/api/GetProductDetailedView.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 
 
-const GetProductDetailedView =async (id) => {
+const GetProductDetailedView = async (id) => {
     return axios.get(`https://dummyjson.com/products/${id}`)
         .then(response => response.data)
-        .catch(error => error.message)
+
 }
 
 export default GetProductDetailedView
